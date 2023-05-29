a3b09fed2990f4980dd715ee1579a4651aa5d91f
7rikaz_wsl1 <7rikaz.h785.stat2ltas41lcijad@gmail.com>
2023-05-29 23:47:59 +0900

    feat: Update code and README.md
    - Added pytest code.
    - Changed opencv-python to ffmpeg for getting the resolution of the input video.
    - Removed opencv-python module from poetry.
    - Imported file renaming and file deletion processes.
    - Deleted requirements.txt file.
    - Added ffmpeg information to README.md.

--- video_grid_merge/delete_files.py
+++ video_grid_merge/delete_files.py
@@ -1,43 +1,41 @@
 import os
+from typing import List
 
 temporarily_data_list = ["_TV", "_LP", ".txt"]
 
 
-def delete_files_in_folder(folder_path):
+def delete_files_in_folder(tmp_data_list: List[str], input_folder: str):
     """Deletes temporary data files in the specified folder.
 
     Args:
+        tmp_data_list (List[str]): List defining temporarily stored data information
         input_folder (str): Path of the folder to be deleted
     """
     files_to_delete = [
         file_name
-        for file_name in os.listdir(folder_path)
-        if any(x in file_name for x in temporarily_data_list)
+        for file_name in os.listdir(input_folder)
+        if any(x in file_name for x in tmp_data_list)
     ]
     for file_name in files_to_delete:
-        file_path = os.path.join(folder_path, file_name)
+        file_path = os.path.join(input_folder, file_name)
         if os.path.isfile(file_path):
             os.remove(file_path)
 
 
-def delete_files_with_confirmation(path):
+def delete_files_with_confirmation(tmp_data_list: List[str], path: str):
     """Ask for confirmation before deleting a file in the specified path.
 
     Args:
+        tmp_data_list (List[str]): List defining temporarily stored data information
         path (str): Path to be deleted
     """
     confirmation = input(
-        f"\nFiles other than input videos were created in the following paths.\nDo you want to delete the created files?[y/N]\n{path}\n"
+        f"\nFiles other than input videos were created in the following paths.\n{path}\nDo you want to delete the created files?[y/N] "
     )
     if confirmation.lower() == "y":
-        delete_files_in_folder(path)
+        delete_files_in_folder(tmp_data_list, path)
 
 
-def main():
-    # Specify the path of the folder you want to delete
+if __name__ == "__main__":  # pragma: no cover
     path = "./video_grid_merge/media/input"
-    delete_files_with_confirmation(path)
-
-
-if __name__ == "__main__":
-    main()
+    delete_files_with_confirmation(temporarily_data_list, path)
